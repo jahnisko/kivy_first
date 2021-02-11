c27c8ee839c7a0d2c1adf89df22272a73b38799b
Vítězslav Jahn <46920987+jahnisko@users.noreply.github.com>
2021-02-11 11:54:58 +0100

    FloatLayout

--- main.py
+++ main.py
@@ -1,49 +1,11 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols = 1
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="First name: "))
-        self.name = TextInput(multiline=False)
-        self.inside.add_widget(self.name)
-
-        self.inside.add_widget(Label(text="Last name: "))
-        self.l_name = TextInput(multiline=False)
-        self.inside.add_widget(self.l_name)
-
-        self.inside.add_widget(Label(text="Email: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    def pressed(self, instance):
-        name = self.name.text
-        last = self.l_name.text
-        email = self.email.text
-
-        print(f"Name: {name}, Last name: {last}, Email: {email}")
-        self.name.text = ""
-        self.l_name.text = ""
-        self.l_name.text = ""
+from kivy.uix.floatlayout import FloatLayout
 
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
 
 
 if __name__ == "__main__":
